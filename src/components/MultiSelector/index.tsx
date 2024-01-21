41b48d0caba29dd503dfcc5fa53d3f8e61321dc8
Mohammad Jalali <muhammad.art79@gmail.com>
2024-01-21 17:44:23 +0330

    add list and work on tsx

--- src/components/MultiSelector/index.tsx
+++ src/components/MultiSelector/index.tsx
@@ -1,25 +1,58 @@
+import React, { useState } from 'react';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ClearIcon from '@mui/icons-material/Clear';
 import "../../styles/multiselect-style.sass"
 
-const MultiSelector = () => {
+const typeList: string[] = ['Education', 'Yeeeah, science!', 'Art', 'Sport', 'Games', 'Health'];
+
+const MultiSelector: React.FC = () => {
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [filteredTypes, setFilteredTypes] = useState<string[]>(typeList);
+    const [selectedList, setSelectedList] = useState<string[]>([]);
+    const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const term = event.target.value;
+        setSearchTerm(term);
+
+        const filtered = typeList.filter(item => item.toLowerCase().includes(event.target.value.toLowerCase()));
+        setFilteredTypes(filtered);
+    };
+
+    const selecting = (selectedItem: string): void => {
+        setSelectedItem(selectedItem);
+        
+        if (!selectedList.includes(selectedItem)) {
+            setSelectedList((prevSelectedList) => [...prevSelectedList, selectedItem]);
+        }
+    };
+
     return (
         <div>
-            <div className="select-box">
-                <input type="text" className='select-input' placeholder='Type Your Type :)' />
-                <ExpandMoreIcon sx={{color: "#4e64c4"}} />
+            <div className='selected-items'>
+                {selectedList.map((item, index) => (
+                    <span className='selected-item' key={index}><span className='text'>{item}</span><span className='close'><ClearIcon sx={{ color: "rgb(150 165 230)" }} /></span></span>
+                ))}
             </div>
-            <div className='select-item'>
+            <div className="select-box">
+                <input
+                    type="text"
+                    className='select-input'
+                    placeholder='Type Your Type :)'
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+                <ExpandMoreIcon sx={{ color: "rgb(150 165 230)" }} />
+            </div>
+            <div className='select-items'>
                 <ul>
-                    <li>Education</li>
-                    <li>Yeeeah, science!</li>
-                    <li>Art</li>
-                    <li>Sport</li>
-                    <li>Games</li>
-                    <li>Health</li>
+                    {filteredTypes.map((item, index) => (
+                        <li onClick={() => selecting(item)} key={index} className={item === selectedItem ? 'selected' : ''}>{item}</li>
+                    ))}
                 </ul>
             </div>
         </div>
     )
 }
 
-export default MultiSelector
+export default MultiSelector;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "./styles/main-style.sass"
+import "../public/fonts/index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
